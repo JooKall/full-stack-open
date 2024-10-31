72d5299b01db3bc97c590fd7eb8edb076c4be7bb
Joonas Kallio <kallio.joonas@proton.me>
2024-10-31 16:06:28 +0200

    8.8-8.12 done

---
+++ osa8/library-frontend/src/components/BirthyearForm.jsx
@@ -0,0 +1,53 @@
+import Select from 'react-select'
+import { useState } from 'react'
+import { useMutation } from '@apollo/client'
+
+import { EDIT_BIRTHYEAR } from '../queries'
+
+const BirthYearForm = ({ authors }) => {
+  const [year, setYear] = useState('')
+  const [selectedOption, setSelectedOption] = useState(null)
+
+  const [changeBirthYear] = useMutation(EDIT_BIRTHYEAR)
+
+  const options = authors.map((author) => ({
+    value: author.name,
+    label: author.name,
+  }))
+
+  const submit = async (event) => {
+    event.preventDefault()
+    if (selectedOption) {
+      changeBirthYear({
+        variables: { name: selectedOption.value, year: Number(year) },
+      })
+      setYear('')
+    }
+  }
+
+  return (
+    <div>
+      <h2>Set birthyear </h2>
+
+      <form onSubmit={submit}>
+        <div>
+          <Select
+            defaultValue={selectedOption}
+            onChange={setSelectedOption}
+            options={options}
+          />
+        </div>
+        <div>
+          born{' '}
+          <input
+            value={year}
+            onChange={({ target }) => setYear(target.value)}
+          />
+        </div>
+        <button type="submit">update author</button>
+      </form>
+    </div>
+  )
+}
+
+export default BirthYearForm
